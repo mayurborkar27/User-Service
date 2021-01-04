883a4178db3843d203737a7da115b0748a0604b2
mayurb1 <mayurb@proximabiz.com>
2021-01-04 18:00:59 +0530

    api integration

--- components/tenants/admin/TenantsDetail.tsx
+++ components/tenants/admin/TenantsDetail.tsx
@@ -27,21 +27,21 @@ export default function TenantsDetail({ id }) {
     const { loading, error, data } = useQuery(ALL_TENANTS_DETAIL);
     console.log(data);
     
-    const client = data.Clients.clients.map((client,index) => {
-        if (id == client.primaryOperatorId ) {
-            return (
-                <Row className="pt-3" key={index} >
-                    <Col>
-                        <img src="/dumy logo.svg" />
-                    </Col>
-                    <Col>{client.name}</Col>
-                    <Col>{client.address.country}</Col>
-                    <Col>{client.contractLink}</Col>
-                    <Col></Col>
-                </Row> 
-            )
-        }
-    })
+    // const client = data.Clients.clients.map((client,index) => {
+    //     if (id == client.primaryOperatorId ) {
+    //         return (
+    //             <Row className="pt-3" key={index} >
+    //                 <Col>
+    //                     <img src="/dumy logo.svg" />
+    //                 </Col>
+    //                 <Col>{client.name}</Col>
+    //                 <Col>{client.address.country}</Col>
+    //                 <Col>{client.contractLink}</Col>
+    //                 <Col></Col>
+    //             </Row> 
+    //         )
+    //     }
+    // })
 
     const user = List.map((userDetail,index) => {
         if (id == userDetail.id) {
@@ -76,8 +76,8 @@ export default function TenantsDetail({ id }) {
                 </Row>
                 <div className={styles.body}>
                     <div>
-                        {/* {user} */}
-                        {client}
+                        {user}
+                        {/* {client} */}
                         <div className={styles.body__btn}>
                             <Button variant="outline-primary" className={styles.btn}>Edit</Button>
                             <Button variant="outline-primary" className={styles.btn}>Delete</Button>
