c7cfe15ed4cdc02a5af4029132c78f1844f6c09a
mayurb1 <mayurb@proximabiz.com>
2021-01-05 13:12:41 +0530

    new tenants page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,9 @@
 import ProfilePage from "../components/profile/ProfilePage";
+import UserProfile from "../components/tenants/user/UserProfile";
 
 export default function Profile() {
     return (
-        <div>
-            <ProfilePage/>
-        </div>
+        // <ProfilePage/> 
+        <UserProfile />
     )
 }
